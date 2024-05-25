817caca11b72863b1b5836040b8a6c044b4ec228
Divya Swarnkar <kdivyaswarnkar@gmail.com>
2024-05-25 07:48:57 +0530

    06 Applying Bootstrap Theme into Application

--- DealDouble.Web/App_Start/BundleConfig.cs
+++ DealDouble.Web/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace DealDouble.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
         }
     }
 }
