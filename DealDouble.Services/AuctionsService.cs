434565a06e5ed7d77849c6c9799d0c547cea716a
Divya Swarnkar <kdivyaswarnkar@gmail.com>
2024-06-07 14:37:08 +0530

    Control Panel Dashboard for Admins / Worked related to Sections

--- DealDouble.Services/AuctionsService.cs
+++ DealDouble.Services/AuctionsService.cs
@@ -1,6 +1,5 @@
 ﻿using DealDouble.Data;
 using DealDouble.Entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
--- DealDouble.Web/Controllers/CategoriesController.cs
+++ DealDouble.Web/Controllers/CategoriesController.cs
@@ -1,10 +1,6 @@
 ﻿using DealDouble.Entities;
 using DealDouble.Services;
 using DealDouble.Web.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace DealDouble.Web.Controllers
